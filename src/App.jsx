43ff83461419028f41328ad035c921704e542d1e
Hafsa Sarker <73364680+HafsaSarker@users.noreply.github.com>
2023-02-28 21:01:27 -0500

    add a counter

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
-
+import React, { useState } from 'react';
 import './App.css'
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [multiplier, setMultiplier] = useState(1);
+
+  function updateCount(){
+    setCount(count + multiplier);
+  }
   return (
     <div className="App">
       <div className="header">
         <h1>Sushi Selector</h1>
-        <h2>Count: </h2>
-        <img src="./sushi.webp" alt="" className="sushi" />
+        <h2>Count: {count}</h2>
+        <img onClick={updateCount} src="./sushi.webp" alt="" className="sushi" />
       </div>
     </div>
   )
